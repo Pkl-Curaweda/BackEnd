1bce1b27864bb39c3cf603ed1249e037e7e65c14
kemalzaki <kemalmuhammadzaki@gmail.com>
2023-11-03 09:53:38 +0700

    update room model according to new erd

---
+++ prisma/seeder/inroomservice/services/room.seeder.js
@@ -0,0 +1,17 @@
+const { prisma } = require("../../config");
+
+const rooms = [
+  {
+    
+  },
+];
+
+async function roomSeed() {
+  for (let room of rooms) {
+    await prisma.room.create({
+      data: room,
+    });
+  }
+}
+
+module.exports = { roomSeed };
