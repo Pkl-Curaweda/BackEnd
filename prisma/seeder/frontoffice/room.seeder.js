9e338fd0192fd00d87984c01d393ae3f64d85288
kemalzaki <kemalmuhammadzaki@gmail.com>
2023-11-03 14:03:37 +0700

    fix bugs in make room seeder, fix placement of files, and make frontOfficeBatchSeed

--- prisma/seeder/frontoffice/room.seeder.js
+++ prisma/seeder/frontoffice/room.seeder.js
@@ -2,7 +2,7 @@ const { prisma } = require("../config");
 
 const rooms = [
   {
-    room_type: STANDARD,
+    room_type: 'STANDARD',
     room_image: "https://i.pravatar.cc/300",
     room_status: 1,
     room_code: 1,
@@ -17,6 +17,22 @@ const rooms = [
     room_capacity_id: 2,
     rate_code: 5.0
   },
+  {
+    room_type: 'STANDARD',
+    room_image: "https://i.pravatar.cc/300",
+    room_status: 1,
+    room_code: 1,
+    category: "well",
+    floor: 3,
+    i: 2,
+    occupied_status: true,
+    overlook: "well",
+    description: "kamar well",
+    bed_setup: "well",
+    connecting: "well",
+    room_capacity_id: 2,
+    rate_code: 5.0
+  }
 ];
 
 async function roomSeed() {
--- prisma/seeder/frontoffice/roomFacility.seeder.js
+++ prisma/seeder/frontoffice/roomFacility.seeder.js
@@ -3,13 +3,13 @@ const { prisma } = require("../config");
 const roomFacilities = [
   {
     name: "Family Room",
-    roomId: "1",
+    roomId: 1,
     created_at: new Date(),
     updated_at: new Date()
   },
   {
     name: "Deluxe Room",
-    roomId: "2",
+    roomId: 2,
     created_at: new Date(),
     updated_at: new Date()
   }
@@ -17,7 +17,7 @@ const roomFacilities = [
 
 async function roomFacilitySeed() {
   for (let roomFacility of roomFacilities) {
-    await prisma.room.create({
+    await prisma.roomFacility.create({
       data: roomFacility,
     });
   }
