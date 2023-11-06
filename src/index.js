ff3f98341431ee17e4d9e36a3599d2ff7a613ac8
Harmoni Natanael S <harmoninatanaels@gmail.com>
2023-11-06 13:02:58 +0700

    feat: adding express and other requirement to build API

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json()) //Used to convert json data
+
+app.listen(port, () => {
+    console.log("Listening to port "+port);
+})
