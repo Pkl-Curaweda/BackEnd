7178c405e65d84956a35075cbbdce80d305c7e4f
Yuddha Wastu <wastupramukha@gmail.com>
2023-11-07 10:59:11 +0700

    seeder Guest Preference, Room Change, Task, Departement

--- prisma/seeder/housekeeping/paket5/index.js
+++ prisma/seeder/housekeeping/paket5/index.js
@@ -1,5 +1,15 @@
+const { guestPreferenceSeed } = require("./guestPreference.seeder");
+const { roomChangeSeed } = require("./roomChange.seeder");
+const { departementSeed } = require("./departement.seeder");
+const { taskSeed } = require("./task.seeder");
+// const { forecast } = require("./forecast.seeder");
+
 async function paketLimaSeed() {
-  // apa saja buat ntar import
+  await guestPreferenceSeed(); // #1
+  await roomChangeSeed(); // #2
+  await departementSeed(); // #3
+  await taskSeed(); // #4
+//  await forecast(); // #5
 }
 
 module.exports = { paketLimaSeed };
