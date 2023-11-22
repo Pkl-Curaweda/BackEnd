025c033d934f836b678ae9789ab5795ce6ed6283
Harmoni Natanael S <harmoninatanaels@gmail.com>
2023-11-22 16:07:14 +0700

    fix: fix schema prisma that didnt match with FO repo note: sorry for the bug in the last commit

--- prisma/seeder/frontoffice/index.js
+++ prisma/seeder/frontoffice/index.js
@@ -3,11 +3,13 @@ const { roomBatchSeed } = require("./room");
 const { ReservationBatchSeed } = require("./reservation");
 const { roomChangeSeed } = require("./roomChange.seeder");
 const { specialTreatmentSeed } = require("./specialTreatment.seeder");
+const { LogAvailability } = require("./logAvailability.seeder");
 
 async function frontOfficeBatchSeed() {
   await guestSeed();
   await roomBatchSeed();
   await ReservationBatchSeed();
+  await LogAvailability();
   await roomChangeSeed();
   await specialTreatmentSeed();
 }
