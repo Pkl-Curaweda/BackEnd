db08e9720bc95adde8106a5716be0a9bef9bb7e9
Najwan Aribena <aribenanajwan@gmail.com>
2023-11-22 10:11:13 +0700

    feat: room status and special treatment seeder

--- prisma/seeder/frontoffice/index.js
+++ prisma/seeder/frontoffice/index.js
@@ -2,12 +2,14 @@ const { guestSeed } = require("./guest.seeder");
 const { roomBatchSeed } = require("./room");
 const { ReservationBatchSeed } = require("./reservation");
 const { roomChangeSeed } = require("./roomChange.seeder");
+const { specialTreatmentSeed } = require("./specialTreatment.seeder");
 
 async function frontOfficeBatchSeed() {
   await guestSeed();
   await roomBatchSeed();
   await ReservationBatchSeed();
   await roomChangeSeed();
+  await specialTreatmentSeed();
 }
 
 module.exports = { frontOfficeBatchSeed };
