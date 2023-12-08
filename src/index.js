cc6bcac2c8a10f3028128407d1c3333fd46ffd17
Diaz-adrianz <diazadrianz191205@gmail.com>
2023-12-09 01:11:08 +0700

    add cors origin for frontend purpose

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const R_Login = require("./routes/R_Login");
 const R_Reservation = require("./routes/R_Reservation");
+const cors = require('cors')
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+const origins = process.env.ALLOWED_ORIGINS || [];
+
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors({
+  origin: origins.split(','),
+  credentials: true
+}))
 
 // app.get("*", checkUser)
 app.use("/auth", R_Login);
