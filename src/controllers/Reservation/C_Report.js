47e3195bcac6f2315f844cb4298115746b4d2ea0
Putri Puspita Kartini Rahman <putripuspitakr@gmail.com>
2023-12-12 09:07:21 +0700

    fix:roomhange

--- src/controllers/Reservation/C_Report.js
+++ src/controllers/Reservation/C_Report.js
@@ -2,8 +2,10 @@ const { getReportData } = require("../../models/Front Office/M_Report");
 const { success, error } = require("../../utils/response");
 
 const getAllReport = async (req, res) => {
-  try {
-    const data = await getReportData();
+  try { 
+    
+    const { disOpt } = req.query
+    const data = await getReportData(disOpt);
     return success(res, 'Operation Success', data)
   } catch (err) {
     return error(res, err.message)
