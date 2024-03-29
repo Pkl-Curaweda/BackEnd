8700a60d216f9da7a7ab44cd931ac0adbb776644
Harmoni Natanael S <harmoninatanaels@gmail.com>
2024-03-29 18:11:54 +0700

    jsdbjad

--- src/controllers/House Keeping/IMPPS/C_RoomMaid.js
+++ src/controllers/House Keeping/IMPPS/C_RoomMaid.js
@@ -8,8 +8,8 @@ const get = async (req, res) => {
     const user = req.user
     try {
         const roomMaid = await isRoomMaid(user.id)
-        const { maidName, performance, listTask } = await getRoomMaidTaskById(roomMaid.id, req.query)
-        return success(res, `Get Success`, { maidName, performance, listTask })
+        const { maidName, aliases, workload,  performance, listTask } = await getRoomMaidTaskById(roomMaid.id, req.query)
+        return success(res, `Get Success`, { maidName, aliases, workload, performance, listTask })
     } catch (err) {
         return error(res, err.message)
     }
